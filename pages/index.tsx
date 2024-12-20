06026485a29099974f66da0a809ca62c31486e2e
Yuri007 <136291503+YuriKauan10@users.noreply.github.com>
2024-12-19 21:31:26 -0300

    adicionando-css-tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,33 @@
 //   variable: "--font-geist-sans",
 //   subsets: ["latin"],
 // });
-
 // const geistMono = Geist_Mono({
 //   variable: "--font-geist-mono",
 //   subsets: ["latin"],
 // });
+const nome = 'Yuri'
+const sobrenome = 'Kauan'
+
+
+function Topo(){
+  const idade = 18
+
+  return (
+    <div className = ' justify-between items-center bg-zinc-300 h-20'>
+      <div className="flex flex-col justify-center items-center">
+      <h1>Meu nome é: </h1>
+      <div className="text-3xl">{nome + ' ' + sobrenome}</div>
+      <div>{idade}</div> 
+      </div>
+      <div className="subtitulo">Programador</div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
-      <main>
-        
+    <main>
+        <Topo/>
       </main>
     )
 }
